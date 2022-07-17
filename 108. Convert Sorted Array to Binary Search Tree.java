c5ba18b37a091e2d8a4d2606e4d7cce2e5e24116
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2022-07-17 12:36:32 +0530

    Create 108. Convert Sorted Array to Binary Search Tree.java

---
+++ 108. Convert Sorted Array to Binary Search Tree.java
@@ -0,0 +1,54 @@
+/*
+Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+
+ 
+
+Example 1:
+
+
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+Example 2:
+
+
+Input: nums = [1,3]
+Output: [3,1]
+Explanation: [1
+
+,null,3] and [3,1] are both height-balanced BSTs.
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums is sorted in a strictly increasing order.
+
+
+
+*/
+
+class Solution {
+    public TreeNode sortedArrayToBST(int[] num) {
+        if (num.length == 0)
+			return null;
+ 
+		return sortedArrayToBST(num, 0, num.length - 1);
+	}
+ 
+	public TreeNode sortedArrayToBST(int[] num, int start, int end) {
+		if (start > end)
+			return null;
+ 
+		int mid = (start + end) / 2;
+		TreeNode root = new TreeNode(num[mid]);
+		root.left = sortedArrayToBST(num, start, mid - 1);
+		root.right = sortedArrayToBST(num, mid + 1, end);
+ 
+		return root;
+    }
+}
