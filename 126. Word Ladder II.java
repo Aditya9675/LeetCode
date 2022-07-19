345e07fb7d3046550541af9d21463d416faf7a4a
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2022-07-19 12:41:27 +0530

    Update 126. Word Ladder II.java

--- 126. Word Ladder II.java
+++ 126. Word Ladder II.java
@@ -37,78 +37,41 @@ All the words in wordList are unique.
 
 
 class Solution {
+    private int min = Integer.MAX_VALUE >> 1;
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-          List<List<String>> result = new ArrayList<List<String>>();
- 
-        HashSet<String> unvisited = new HashSet<>();
-        unvisited.addAll(wordList);
- 
-        LinkedList<Node> queue = new LinkedList<>();
-        Node node = new Node(beginWord,0,null);
-        queue.offer(node);
- 
-        int minLen = Integer.MAX_VALUE;
-        while(!queue.isEmpty()){
-            Node top = queue.poll();
- 
-            
-            if(result.size()>0 && top.depth>minLen){
-                return result;
-            }
- 
-            for(int i=0; i<top.word.length(); i++){
-                char c = top.word.charAt(i);
-                char[] arr = top.word.toCharArray();
-                for(char j='z'; j>='a'; j--){
-                    if(j==c){
-                        continue;
-                    }
-                    arr[i]=j;
-                    String t = new String(arr);
- 
-                    if(t.equals(endWord)){
-                       
-                        List<String> aResult = new ArrayList<>();
-                        aResult.add(endWord);
-                        Node p = top;
-                        while(p!=null){
-                            aResult.add(p.word);
-                            p = p.prev;
-                        }
- 
-                        Collections.reverse(aResult);
-                        result.add(aResult);
- 
-                       
-                        if(top.depth<=minLen){
-                            minLen=top.depth;
-                        }else{
-                            return result;
-                        }
-                    }
- 
-                    if(unvisited.contains(t)){
-                        Node n=new Node(t,top.depth+1,top);
-                        queue.offer(n);
-                        unvisited.remove(t);
-                    }
-                }
-            }
-        }
- 
-        return result;
-    }
-}
- 
-class Node{
-    public String word;
-    public int depth;
-    public Node prev;
- 
-    public Node(String word, int depth, Node prev){
-        this.word=word;
-        this.depth=depth;
-        this.prev=prev;
         
+        List<List<String>> result = new ArrayList<>();
+ 		Set<String> words = new HashSet<>(wordList);
+ 		if(!words.contains(endWord)) return result;
+ 		int len = beginWord.length();
+ 		List<String> temp = new ArrayList<String>();
+ 		temp.add(beginWord);
+ 		dfs(result, words, len, endWord, beginWord, temp);
+ 		return result;
+ 	}
+ 	private void dfs(List<List<String>> result, Set<String> words, int len, String endWord, String cur, List<String> temp){
+ 		if(cur.equals(endWord)){
+ 			if(temp.size() < min){
+ 				result.clear();
+ 				min = temp.size();
+ 			}
+ 			result.add(new ArrayList<String>(temp));
+ 		}else if(temp.size() < min){
+ 			for(int i = 0; i < len; i++){
+ 				char[] arr = cur.toCharArray();
+ 				for(char c = 'a'; c <= 'z'; c++){
+ 					arr[i] = c;
+ 					String next = new String(arr);
+ 					if(words.contains(next)){
+ 						words.remove(next);
+ 						temp.add(next);
+ 						dfs(result, words, len, endWord, next, temp);
+ 						temp.remove(temp.size() - 1);
+ 						words.add(next);
+ 					}
+ 				}
+ 				arr[i] = cur.charAt(i);
+ 			}
+ 		}
     }
 }
