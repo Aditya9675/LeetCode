f29eab3ef96e25cc75b9606744a042d2c4f84783
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2022-07-20 23:16:28 +0530

    Update 160. Intersection of Two Linked Lists.java

--- 160. Intersection of Two Linked Lists.java
+++ 160. Intersection of Two Linked Lists.java
@@ -60,49 +60,18 @@ intersectVal == listA[skipA] == listB[skipB] if listA and listB intersect.
 
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        int len1 = 0;
-        int len2 = 0;
-        ListNode p1=headA, p2=headB;
-        if (p1 == null || p2 == null)
-            return null;
- 
-        while(p1 != null){
-            len1++;
-            p1 = p1.next;
+         if(headA == null || headB == null) return null;
+        ListNode a = headA;
+        ListNode b = headB;
+        int count = 0;
+        while(a != b){
+            if(count == 2) break;
+            if(a == null) count ++;
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
         }
-        while(p2 !=null){
-            len2++;
-            p2 = p2.next;
-        }
- 
-        int diff = 0;
-        p1=headA;
-        p2=headB;
- 
-        if(len1 > len2){
-            diff = len1-len2;
-            int i=0;
-            while(i<diff){
-                p1 = p1.next;
-                i++;
-            }
-        }else{
-            diff = len2-len1;
-            int i=0;
-            while(i<diff){
-                p2 = p2.next;
-                i++;
-            }
-        }
- 
-        while(p1 != null && p2 != null){
-            if(p1.val == p2.val){
-                return p1;
-            }
-            p1 = p1.next;
-            p2 = p2.next;
-        }
- 
-        return null;
+        if(a != b) return null;
+        else
+            return a;
     }
 }
