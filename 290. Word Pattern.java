54a734b8b040e897b50d569a18c47d9148863b67
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2022-07-21 23:40:35 +0530

    Create 290. Word Pattern.java

---
+++ 290. Word Pattern.java
@@ -0,0 +1,59 @@
+/*
+
+Given a pattern and a string s, find if s follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+
+ 
+
+Example 1:
+
+Input: pattern = "abba", s = "dog cat cat dog"
+Output: true
+Example 2:
+
+Input: pattern = "abba", s = "dog cat cat fish"
+Output: false
+Example 3:
+
+Input: pattern = "aaaa", s = "dog cat cat dog"
+Output: false
+ 
+
+Constraints:
+
+1 <= pattern.length <= 300
+pattern contains only lower-case English letters.
+1 <= s.length <= 3000
+s contains only lowercase English letters and spaces ' '.
+s does not contain any leading or trailing spaces.
+All the words in s are separated by a single space.
+
+*/
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+         String[] arr = s.split(" ");  
+ 
+    
+    if(arr.length != pattern.length())
+        return false;
+ 
+    HashMap<Character, String> map = new HashMap<Character, String>();
+    for(int i=0; i<pattern.length(); i++){
+        char c = pattern.charAt(i);
+        if(map.containsKey(c)){
+            String value = map.get(c);
+            if(!value.equals(arr[i])){
+                return false;
+            }
+        }else if (map.containsValue(arr[i])){
+            return false;
+        }
+        map.put(c, arr[i]);
+    }
+ 
+    return true;
+    }
+}
+
