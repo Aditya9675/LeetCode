fac8afa36b765ba2999f50ee38e9b198bff0bbf3
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2022-07-23 23:59:37 +0530

    Create 221. Maximal Square.java

---
+++ 221. Maximal Square.java
@@ -0,0 +1,67 @@
+/*
+
+Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+Output: 4
+Example 2:
+
+
+Input: matrix = [["0","1"],["1","0"]]
+Output: 1
+Example 3:
+
+Input: matrix = [["0"]]
+Output: 0
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 300
+matrix[i][j] is '0' or '1'.
+
+*/
+
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+         if(matrix==null||matrix.length==0){
+        return 0;
+    }
+ 
+    int result=0;
+    int[][] dp = new int[matrix.length][matrix[0].length];
+ 
+    for(int i=0; i<matrix.length; i++){
+        dp[i][0]=matrix[i][0]-'0';
+        result=Math.max(result, dp[i][0]);
+    }
+ 
+    for(int j=0; j<matrix[0].length; j++){
+        dp[0][j]=matrix[0][j]-'0';
+        result=Math.max(result, dp[0][j]);
+    }
+ 
+    for(int i=1; i<matrix.length; i++){
+        for(int j=1; j<matrix[0].length; j++){
+            if(matrix[i][j]=='1'){
+                int min = Math.min(dp[i-1][j], dp[i][j-1]);
+                min = Math.min(min, dp[i-1][j-1]);
+                dp[i][j]=min+1;
+ 
+                result = Math.max(result, min+1);
+            }else{
+                dp[i][j]=0;
+            }    
+        }
+    }
+ 
+    return result*result;
+    }
+}
