dc3fec9e66e12cba65ee615ab12aa028eee7982b
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2022-07-23 23:57:57 +0530

    Update 220. Contains Duplicate III.java

--- 220. Contains Duplicate III.java
+++ 220. Contains Duplicate III.java
@@ -28,23 +28,26 @@ Constraints:
 
 class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        if (k < 1 || t < 0)
-		return false;
+        if(nums==null||nums.length<2||k<0||t<0)
+        return false;
  
-	TreeSet<Integer> set = new TreeSet<Integer>();
+    TreeSet<Long> set = new TreeSet<Long>();
+    for(int i=0; i<nums.length; i++){
+        long curr = (long) nums[i];
  
-	for (int i = 0; i < nums.length; i++) {
-		int c = nums[i];
-		if ((set.floor(c) != null && c <= set.floor(c) + t)
-		|| (set.ceiling(c) != null && c >= set.ceiling(c) -t))
-			return true;
+        long leftBoundary = (long) curr-t;
+        long rightBoundary = (long) curr+t+1; 
+        SortedSet<Long> sub = set.subSet(leftBoundary, rightBoundary);
+        if(sub.size()>0)
+            return true;
  
-		set.add(c);
+        set.add(curr);   
  
-		if (i >= k)
-			set.remove(nums[i - k]);
-	}
+        if(i>=k){ 
+            set.remove((long)nums[i-k]);
+        }
+    }
  
-	return false;
+    return false;
     }
 }
