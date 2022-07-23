f667b3f340b2b633759b63ff63602b6392981fb5
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2022-07-23 22:26:09 +0530

    Update 315. Count of Smaller Numbers After Self.java

--- 315. Count of Smaller Numbers After Self.java
+++ 315. Count of Smaller Numbers After Self.java
@@ -30,37 +30,32 @@ Constraints:
 -104 <= nums[i] <= 104
 
 */
-
 class Solution {
-    public List<Integer> countSmaller(int[] nums) {
-       Map<Integer, Integer> map = new HashMap<>();
-        int[] arr = new int[nums.length];
-        for(int i = nums.length - 1; i>= 0; i--){
-            if(map.containsKey(nums[i])){
-                arr[i] = map.get(nums[i]);
-            }
-            else{
-                arr[i] = -1;
-            }
-            map.put(nums[i], i);
-        }
-        int[] result = new int[nums.length];
-        for(int i = nums.length - 1; i >= 0; i--){
-            int count = 0;
-            if(arr[i] == -1){
-                for(int j = i; j < nums.length; j++){
-                    if(nums[i] > nums[j]) count++;
-                }
-                result[i] = count;
-            }else{
-                for(int j = i; j < arr[i]; j++){
-                    if(nums[i] > nums[j]) count++;
-                }
-                result[i] = count + result[arr[i]];
-            }
-        }
-        List<Integer> res = new LinkedList<>();
-        for(int n : result) res.add(n);
-        return res;
+     public List<Integer> countSmaller(int[] nums) {
+    Integer[] ans = new Integer[nums.length];
+    List<Integer> sorted = new ArrayList<Integer>();
+    for (int i = nums.length - 1; i >= 0; i--) {
+        int index = findIndex(sorted, nums[i]);
+        ans[i] = index;
+        sorted.add(index, nums[i]);
+    }
+    return Arrays.asList(ans);
+}
+private int findIndex(List<Integer> sorted, int target) {
+    if (sorted.size() == 0) return 0;
+    int start = 0;
+    int end = sorted.size() - 1;
+    if (sorted.get(end) < target) return end + 1;
+    if (sorted.get(start) >= target) return 0;
+    while (start + 1 < end) {
+        int mid = start + (end - start) / 2;
+        if (sorted.get(mid) < target) {
+            start = mid + 1;
+        } else {
+            end = mid;
         }
     }
+    if (sorted.get(start) >= target) return start;
+    return end;
+}
+}
