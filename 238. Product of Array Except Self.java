f5ba6f6c3faf170f89fa9f2549a9901eeae8dc7c
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2022-07-24 22:00:31 +0530

    Create 238. Product of Array Except Self.java

---
+++ 238. Product of Array Except Self.java
@@ -0,0 +1,56 @@
+/*
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+ 
+
+Constraints:
+
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+*/
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+    
+        int[] result = new int[nums.length];
+ 
+    int[] t1 = new int[nums.length];
+    int[] t2 = new int[nums.length];
+ 
+    t1[0]=1;
+    t2[nums.length-1]=1;
+ 
+   
+    for(int i=0; i<nums.length-1; i++){
+        t1[i+1] = nums[i] * t1[i];
+    }
+ 
+ 
+    for(int i=nums.length-1; i>0; i--){
+        t2[i-1] = t2[i] * nums[i];
+    }
+ 
+    
+    for(int i=0; i<nums.length; i++){
+        result[i] = t1[i] * t2[i];
+    }
+ 
+    return result;
+    }
+}
