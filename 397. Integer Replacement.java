7f79dda135dd34ae6643ee5140406dfa8f27ed5d
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2022-07-25 15:58:37 +0530

    Create 397. Integer Replacement.java

---
+++ 397. Integer Replacement.java
@@ -0,0 +1,50 @@
+/*
+
+Given a positive integer n, you can apply one of the following operations:
+
+If n is even, replace n with n / 2.
+If n is odd, replace n with either n + 1 or n - 1.
+Return the minimum number of operations needed for n to become 1.
+
+ 
+
+Example 1:
+
+Input: n = 8
+Output: 3
+Explanation: 8 -> 4 -> 2 -> 1
+Example 2:
+
+Input: n = 7
+Output: 4
+Explanation: 7 -> 8 -> 4 -> 2 -> 1
+or 7 -> 6 -> 3 -> 2 -> 1
+Example 3:
+
+Input: n = 4
+Output: 2
+ 
+
+Constraints:
+
+1 <= n <= 231 - 1
+
+*/
+
+class Solution {
+    public int integerReplacement(int n) {
+         return cost(n,0);
+    }
+    public int cost(int n, int replacement){
+        if(n<0) n=Math.abs(n); 
+        if(n==1) return replacement;
+        if(n==2) return cost(1,replacement+1);
+        if(n%2==0) {
+            n=n/2;
+            return cost(n,replacement+1);
+        }
+        else{
+            return Math.min(cost(n+1,replacement+1),cost(n-1,replacement+1));
+        }
+    }
+}
