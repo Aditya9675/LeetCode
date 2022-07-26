2e05f40d22b8a53ccabdfdce2eb85c0b30dcd9d8
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2022-07-26 22:32:05 +0530

    Create 380. Insert Delete GetRandom O(1).java

---
+++ 380. Insert Delete GetRandom O(1).java
@@ -0,0 +1,96 @@
+/*
+
+Implement the RandomizedSet class:
+
+RandomizedSet() Initializes the RandomizedSet object.
+bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
+bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise.
+int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned.
+You must implement the functions of the class such that each function works in average O(1) time complexity.
+
+ 
+
+Example 1:
+
+Input
+["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"]
+[[], [1], [2], [2], [], [1], [2], []]
+Output
+[null, true, false, true, 2, true, false, 2]
+
+Explanation
+RandomizedSet randomizedSet = new RandomizedSet();
+randomizedSet.insert(1); // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+randomizedSet.remove(2); // Returns false as 2 does not exist in the set.
+randomizedSet.insert(2); // Inserts 2 to the set, returns true. Set now contains [1,2].
+randomizedSet.getRandom(); // getRandom() should return either 1 or 2 randomly.
+randomizedSet.remove(1); // Removes 1 from the set, returns true. Set now contains [2].
+randomizedSet.insert(2); // 2 was already in the set, so return false.
+randomizedSet.getRandom(); // Since 2 is the only number in the set, getRandom() will always return 2.
+ 
+
+Constraints:
+
+-231 <= val <= 231 - 1
+At most 2 * 105 calls will be made to insert, remove, and getRandom.
+There will be at least one element in the data structure when getRandom is called.
+
+
+*/
+
+class RandomizedSet {
+    HashMap<Integer, Integer> valueMap;
+    HashMap<Integer, Integer> idxMap;
+ 
+    
+    public RandomizedSet() {
+        valueMap = new HashMap<>();
+        idxMap = new HashMap<>();
+    }
+ 
+    
+    public boolean insert(int val) {
+        if(valueMap.containsKey(val)){
+            return false;
+        }
+ 
+        valueMap.put(val, valueMap.size());
+        idxMap.put(idxMap.size(), val);
+ 
+        return true;
+    }
+ 
+    public boolean remove(int val) {
+        if(valueMap.containsKey(val)){
+            int idx = valueMap.get(val);
+            valueMap.remove(val);
+            idxMap.remove(idx);
+ 
+            Integer tailElem = idxMap.get(idxMap.size());
+            if(tailElem!=null){
+                idxMap.put(idx,tailElem);
+                valueMap.put(tailElem, idx);
+            }
+ 
+            return true;
+        }
+ 
+        return false;
+    }
+ 
+  
+    public int getRandom() {
+        if(valueMap.size()==0){
+            return -1;
+        }
+ 
+        if(valueMap.size()==1){
+            return idxMap.get(0);
+        }
+ 
+        Random r = new Random();
+        int idx = r.nextInt(valueMap.size());
+ 
+        return idxMap.get(idx);
+    }
+}
