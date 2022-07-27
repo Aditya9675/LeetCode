16407cd8d984f32c32d86e2f7eda3b50329107d6
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2022-07-27 23:18:15 +0530

    Update 349. Intersection of Two Arrays.java

--- 349. Intersection of Two Arrays.java
+++ 349. Intersection of Two Arrays.java
@@ -24,29 +24,24 @@ Constraints:
 
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-    Arrays.sort(nums2);
-    ArrayList<Integer> list = new ArrayList<Integer>();
-    int p1=0, p2=0;
-    while(p1<nums1.length && p2<nums2.length){
-        if(nums1[p1]<nums2[p2]){
-            p1++;
-        }else if(nums1[p1]>nums2[p2]){
-            p2++;
-        }else{
-            list.add(nums1[p1]);
-            p1++;
-            p2++;
- 
+       HashSet<Integer> set = new HashSet<>();
+        ArrayList<Integer> intersection = new ArrayList<>();
+        
+        for (int i = 0; i < nums1.length; i++) {
+            set.add(nums1[i]);
         }
-    }
- 
-    int[] result = new int[list.size()];
-    int i=0;
-    while(i<list.size()){
-        result[i]=list.get(i);
-        i++;
-    }
-    return result;
+        
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.remove(nums2[i])) {
+                intersection.add(nums2[i]);
+            }
+        }
+        
+        int[] result = new int[intersection.size()];
+        for (int i = 0; i < intersection.size(); i++) {
+            result[i] = intersection.get(i);
+        }
+        
+        return result;
     }
 }
