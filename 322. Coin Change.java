e76dfda6d3e1775a4c7187f13ca4f44739912515
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2022-07-28 21:08:09 +0530

    Create 322. Coin Change.java

---
+++ 322. Coin Change.java
@@ -0,0 +1,57 @@
+/*
+
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+You may assume that you have an infinite number of each kind of coin.
+
+ 
+
+Example 1:
+
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+Example 2:
+
+Input: coins = [2], amount = 3
+Output: -1
+Example 3:
+
+Input: coins = [1], amount = 0
+Output: 0
+ 
+
+Constraints:
+
+1 <= coins.length <= 12
+1 <= coins[i] <= 231 - 1
+0 <= amount <= 104
+
+*/
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if(amount==0){
+        return 0;
+    }
+ 
+    int[] dp = new int[amount+1];
+ 
+    Arrays.fill(dp, Integer.MAX_VALUE);
+    dp[0]=0;
+ 
+    for(int i=1; i<=amount; i++){
+        for(int coin: coins){
+            if(i==coin){
+                dp[i]=1;
+            }else if(i>coin){
+                if(dp[i-coin]==Integer.MAX_VALUE){
+                    continue;
+                }
+                dp[i]=Math.min(dp[i-coin]+1, dp[i]);
+            }
+        }
+    }
+ 
