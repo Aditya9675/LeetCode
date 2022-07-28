64a50dde3489634ce06d059a3227461e09367c48
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2022-07-28 21:19:34 +0530

    Update 313. Super Ugly Number.java

--- 313. Super Ugly Number.java
+++ 313. Super Ugly Number.java
@@ -32,25 +32,35 @@ All the values of primes are unique and sorted in ascending order.
 
 class Solution {
     public int nthSuperUglyNumber(int n, int[] primes) {
-         int[] times = new int[primes.length];
-    int[] result = new int[n];
-    result[0] = 1; 
- 
-    for (int i = 1; i < n; i++) {
-        int min = Integer.MAX_VALUE;
-        for (int j = 0; j < primes.length; j++) {
-            min = Math.min(min, primes[j] * result[times[j]]);
-        }
- 
-        result[i] = min;
- 
-        for (int j = 0; j < times.length; j++) {
-            if (result[times[j]] * primes[j] == min) {
-                times[j]++;
+        int m = primes.length;
+        
+        int[] mul = new int[m];
+        
+        Arrays.fill(mul, 0);
+        
+        int[] dp = new int[n];
+        
+        dp[0] = 1;
+        
+        for (int i = 1; i < n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            
+            int temp1 = -1;
+            
+            for (int j = 0; j < m; j++) {
+                int temp2 = dp[mul[j]] * primes[j];
+                
+                if (dp[i] > temp2) {
+                    dp[i] = temp2;
+                    
+                    temp1 = j;
+                } else if (dp[i] == temp2)
+                    mul[j] = mul[j] + 1;
             }
+            
+            mul[temp1] = mul[temp1] + 1;
         }
-    }
- 
-    return result[n - 1];
+        
+        return dp[n - 1];
     }
 }
