73c34db53b0ccd5467284f11395f5b97accda775
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2022-10-09 22:48:15 +0530

    Create 653. Two Sum IV - Input is a BST.java

---
+++ 653. Two Sum IV - Input is a BST.java
@@ -0,0 +1,44 @@
+/*
+
+Given the root of a Binary Search Tree and a target number k, return true if there exist two elements in the BST such that their sum is equal to the given target.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,3,6,2,4,null,7], k = 9
+Output: true
+Example 2:
+
+
+Input: root = [5,3,6,2,4,null,7], k = 28
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-104 <= Node.val <= 104
+root is guaranteed to be a valid binary search tree.
+-105 <= k <= 105
+
+*/
+
+class Solution{
+Set set=new HashSet<Integer>();
+    public boolean findTarget(TreeNode root, int k){
+        return helper(root, k);
+    }
+
+    private boolean helper(TreeNode root,int k){
+        if(root==null){
+            return false;
+        }
+        if(set.contains(k-root.val)){
+            return true;
+        }
+        set.add(root.val);
+        return helper(root.left, k)||helper(root.right, k);
+    }
+}
