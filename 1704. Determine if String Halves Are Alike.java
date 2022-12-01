a415cd5ace173915c6b8efc3ddeeafb569b23999
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2022-12-01 20:30:27 +0530

    Create 1704. Determine if String Halves Are Alike.java

---
+++ 1704. Determine if String Halves Are Alike.java
@@ -0,0 +1,64 @@
+/*
+
+You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
+
+Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
+
+Return true if a and b are alike. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: s = "book"
+Output: true
+Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. Therefore, they are alike.
+Example 2:
+
+Input: s = "textbook"
+Output: false
+Explanation: a = "text" and b = "book". a has 1 vowel whereas b has 2. Therefore, they are not alike.
+Notice that the vowel o is counted twice.
+ 
+
+Constraints:
+
+2 <= s.length <= 1000
+s.length is even.
+s consists of uppercase and lowercase letters.
+
+*/
+
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        int vowelCount=0;
+		int halfSize=s.length()/2;
+
+		for(int i=0;i<halfSize;++i)
+		{
+			char c=s.charAt(i);
+
+			if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+			{
+				++vowelCount;
+			}
+		}
+
+		for(int i=halfSize;i<s.length();++i)
+		{
+			char c = s.charAt(i);
+
+			if(c=='a'||c=='e'||c =='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c== 'O'||c=='U')
+			{
+				--vowelCount;
+
+				if(vowelCount<0)
+				{
+					return false;
+				}
+			}
+		}
+
+		return vowelCount==0;
+    }
+}
