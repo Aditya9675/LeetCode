1950b40a7e7b98698c587a0a3d15609873d2b885
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2023-02-03 21:43:42 +0530

    Create 697. Degree of an Array.java

---
+++ 697. Degree of an Array.java
@@ -0,0 +1,55 @@
+/*
+Given a non-empty array of non-negative integers nums, the degree of this array is defined as the maximum frequency of any one of its elements.
+
+Your task is to find the smallest possible length of a (contiguous) subarray of nums, that has the same degree as nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,2,3,1]
+Output: 2
+Explanation: 
+The input array has a degree of 2 because both elements 1 and 2 appear twice.
+Of the subarrays that have the same degree:
+[1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
+The shortest length is 2. So return 2.
+Example 2:
+
+Input: nums = [1,2,2,3,1,4,2]
+Output: 6
+Explanation: 
+The degree is 3 because the element 2 is repeated 3 times.
+So [2,2,3,1,4,2] is the shortest subarray, therefore returning 6.
+ 
+
+Constraints:
+
+nums.length will be between 1 and 50,000.
+nums[i] will be an integer between 0 and 49,999.
+
+*/
+
+class Solution {
+    public int findShortestSubArray(int[] nums) {
+    int ans = 0;
+    int degree = 0;
+    Map<Integer, Integer> debut = new HashMap<>();
+    Map<Integer, Integer> count = new HashMap<>();
+
+    for(int i = 0; i < nums.length; ++i){
+      final int num = nums[i];
+      debut.putIfAbsent(num, i);
+      count.put(num, count.getOrDefault(num, 0) + 1);
+      if(count.get(num) >degree){
+        degree = count.get(num);
+        ans = i - debut.get(num) + 1;
+      } 
+      else if(count.get(num) == degree){
+        ans = Math.min(ans, i - debut.get(num) + 1);
+      }
+    }
+
+    return ans;    
+    }
+}
