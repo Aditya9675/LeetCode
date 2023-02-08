5131eecdc7ad0febaa4295a8076bca02d88b6613
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2023-02-08 21:26:38 +0530

    Create 693. Binary Number with Alternating Bits.java

---
+++ 693. Binary Number with Alternating Bits.java
@@ -0,0 +1,39 @@
+/*
+
+Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits will always have different values.
+
+ 
+
+Example 1:
+
+Input: n = 5
+Output: true
+Explanation: The binary representation of 5 is: 101
+Example 2:
+
+Input: n = 7
+Output: false
+Explanation: The binary representation of 7 is: 111.
+Example 3:
+
+Input: n = 11
+Output: false
+Explanation: The binary representation of 11 is: 1011.
+ 
+
+Constraints:
+
+1 <= n <= 231 - 1
+
+*/
+
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+     String bits = Integer.toBinaryString(n);
+        for(int i=0; i<bits.length()-1;i++){
+            if(bits.charAt(i) == bits.charAt(i+1)) 
+            return false;
+        }
+        return true;    
+    }
+}
